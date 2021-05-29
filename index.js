daab022dd6e727b561bc290c7ff740d62dd42885
jjjjonathan <hornathanjon@gmail.com>
2021-05-28 22:12:17 -0500

    create basic server

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http
+  .createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end('Hello World!');
+  })
+  .listen(8080);
