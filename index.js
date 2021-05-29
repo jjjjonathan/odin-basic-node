adf3a4099e70ce0197995a51cf8cc3e4d2b89b2d
jjjjonathan <hornathanjon@gmail.com>
2021-05-28 22:55:21 -0500

    complete

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const http = require('http');
+const fs = require('fs');
 
 http
-  .createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    response.end('Hello World!');
+  .createServer((req, res) => {
+    const path = req.url;
+
+    if (path === '/style.css') {
+      fs.readFile('style.css', (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(data);
+        return res.end();
+      });
+    } else if (path === '/' || path === '/about' || path === '/contact-me') {
+      const filename = path === '/' ? './index.html' : `.${req.url}.html`;
+      fs.readFile(filename, (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    } else {
+      fs.readFile('404.html', (err, data) => {
+        if (err) throw err;
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write(data);
+        return res.end();
+      });
+    }
   })
   .listen(8080);
